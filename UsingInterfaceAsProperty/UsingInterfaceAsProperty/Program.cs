71226346ddb5fc86f390712728fa03d4d163b85b
Fathi <chbnfathi@hotmail.com>
2021-08-24 17:34:34 +0200

    Creating the interface and intermediate class.

--- UsingInterfaceAsProperty/UsingInterfaceAsProperty/Program.cs
+++ UsingInterfaceAsProperty/UsingInterfaceAsProperty/Program.cs
@@ -4,9 +4,40 @@ namespace UsingInterfaceAsProperty
 {
     class Program
     {
+
+        public interface IProtectedPsk
+        {
+            string Key{ get; set; }
+            string Value{ get; set; }
+
+        }
+
+
+        public class ProtectedSections
+        {
+            public IProtectedPsk SecurityA { get; set; }
+            public IProtectedPsk SecurityB { get; set; }
+            public IProtectedPsk SecurityC { get; set; }
+        }
+
+
+        //A need of an intermediate class type to performe W/R
+        public class ReadWrite : IProtectedPsk
+        {
+            public string Key { get; set ; }
+            public string Value { get ; set ; }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var sections = new ProtectedSections();
+            sections.SecurityA = new ReadWrite();
+
+            sections.SecurityA.Key = "001";
+            sections.SecurityA.Value = "Sky";
+
+            Console.WriteLine($"{sections.SecurityA.Key} | {sections.SecurityA.Value}");
         }
     }
 }
